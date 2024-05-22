bfe1a524369512c5a4536aef7223cfece82be78e
meelimets <130299285+meelimets@users.noreply.github.com>
2024-05-22 23:15:47 +0300

    Update Subset_Data.py

--- Subset_Data.py
+++ Subset_Data.py
@@ -1,1 +1,18 @@
+# Importing libaries
+import pandas as pd
 
+# Reading data file
+data=pd.read_csv("DataGBIF_Final.csv")
+
+# Defining columns to keep
+columns_to_keep = [
+    'gbifID', 'MNIMI', 'MKOOD', 'iso_code', 'kingdom', 'family',
+    'genus', 'species', 'locality', 'individualCount', 'decimalLatitude',
+    'decimalLongitude', 'eventDate', 'day', 'month', 'year'
+]
+
+# Subsetting the data based on columns
+subset_df = data[columns_to_keep]
+
+# Saving file as .csv file
+subset_df.to_csv('DataGBIF_Final_smallV2.csv', index=False)
